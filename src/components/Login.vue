eab16e6b3d99645c576cd8418b9550fc46242bfd
Jehko <krelude.jko@gmail.com>
2021-08-20 14:38:21 +0900

    불필요 코드 제거, vue-router 설치, 세팅

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-form">Login.vue</div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.login-form {
+  margin: 0 auto;
+  width: 320px;
+  height: 480px;
+  border: 1px solid red;
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+    Main.vue
+    <Login></Login>
+  </div>
+</template>
+
+<script>
+import Login from "@/components/Login.vue"
+
+export default {
+  name: 'Main',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+.main {
+  width: 800px;
+  height: 600px;
+  border: 1px solid black;
+}
+</style>
