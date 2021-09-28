47e92e7a8af23dfefa4fda667f6d91ee4f460b67
Jehko <krelude.jko@gmail.com>
2021-09-28 17:11:30 +0900

    axios 추가, Login 폼 생성

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,41 @@
 <template>
-  <div class="login-form">Login.vue</div>
+  <div class="login-form">Login.vue
+    <form @submit.prevent="onSubmit">
+      <div>
+        <label for="login-id">id</label>
+        <input id="login-id" type="text" v-model="id" />
+      </div>
+      <div>
+        <label for="pw">pw</label>
+        <input id="login-pw" type="password" v-model="password" />
+      </div>
+      <button type="submit">로그인</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Login',
+  data: () => ({
+    id: '',
+    password: ''
+  }),
   components: {
     
+  },
+  methods: {
+    onSubmit () {
+      const payload = { id: this.id, password: this.password }
+      // action="http://localhost:8080/api/v1/auth/login"
+
+      this.axios.post('http://localhost:8080/api/v1/auth/login', payload).then(res => {
+        console.log(res.data);
+        this.axios.get('http://localhost:8080/api/v1/member/myinfo', {headers: {Authorization: 'Bearer ' + res.data.token}}).then(rs => {
+          console.log(rs.data);
+        });
+      })
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import axios from "axios";
 
 const app = createApp(App);
+app.config.globalProperties.axios = axios;
 app.use(router);
 app.mount("#app");
